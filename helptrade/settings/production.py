b8593333d7e058495cbc83557c7fc1ca69eea94f
Illya <illyamazhara1@gmail.com>
2022-12-03 18:42:39 +0200

    Настройки для Хероку

--- helptrade/settings/production.py
+++ helptrade/settings/production.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (BASE_DIR / 'static', )
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
