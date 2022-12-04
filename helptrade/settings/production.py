2203172436a20ec0beb60f54aa7c5e64c3ed94cf
Illya <illyamazhara1@gmail.com>
2022-12-04 17:31:07 +0200

    Настройки для Хероку

--- helptrade/settings/production.py
+++ helptrade/settings/production.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ["helptrade.herokuapp.com/"]
 ALLOWED_HOSTS = ["helptrade.herokuapp.com"]
